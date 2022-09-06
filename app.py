6483906d21165d998fb17f03332b249dcd59d064
Arek Lewicki <lewicki1arek@gmail.com>
2022-09-06 16:07:34 +0200

    Fix app and modify templates

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
 from flask import Flask
-
+from flask import render_template
+from flask import request, redirect
+from flask import url_for
 
 app = Flask(__name__)
 
+@app.route("/mypage/me")
+def mypage():
+    return render_template('me.html')
 
-@app.route('/home')
-def index():
-    return '<h1>Manchester United 3-1 Arsenal</h2>'
-    
+
+@app.route("/mypage/contact", methods=["POST", "GET"])
+def contact():
+    if request.method == "POST":
+        user = request.form['nm']
+        return redirect(url_for("user", usr=user))
+    else:
+        return render_template("contact.html")
+
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>{usr}</h1>"        
+
